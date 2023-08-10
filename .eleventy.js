498cd4455e2b0f078de12795c8d91f35372f64f5
tap1on <ch@leadcapdigital.com>
2023-08-10 11:08:28 +0200

    Added sitemap

--- .eleventy.js
+++ .eleventy.js
@@ -1,11 +1,13 @@
 /* .eleventy.js */
 
 const Image = require("@11ty/eleventy-img");
+const { DateTime } = require("luxon");
 
 module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy("src/assets/css/style.css");
   eleventyConfig.addPassthroughCopy("src/assets/images");
   eleventyConfig.addPassthroughCopy({"src/robots.txt": "/robots.txt"});
+
 
     eleventyConfig.addCollection("page", function(collections) {
         return collections.getFilteredByTag("page").sort(function(a, b) {
@@ -20,6 +22,10 @@ module.exports = function(eleventyConfig) {
       `<h1>${title}</h1>
         <p>${subtitle}</p>`
   );
+
+  eleventyConfig.addShortcode("currentDate", (date = DateTime.now()) => {
+    return date;
+  })
 
 
   return {
